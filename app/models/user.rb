51c5a1b25a84499be7ac74ccf48d342fb1407941
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-07-10 00:25:35 +0100

    Add post model and authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,24 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   attr_accessor :remember_token
+  has_many :posts
 
   has_secure_password
 
-  def User.digest(string)
+  def self.digest(string)
     cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
                                                   BCrypt::Engine.cost
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest)
+                    .is_password?(remember_token)
+  end
+
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 
   def remember
