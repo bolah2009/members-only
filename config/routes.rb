a7941cee634196854b5a0375c9973abd16eb85b2
Buari Bola Ahmed <bola_ahmed2002@yahoo.com>
2019-07-10 01:47:34 +0100

    Update routes configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'posts#index'
+  root to: 'posts#index'
   get     '/login', to: 'sessions#new'
   post    '/login', to: 'sessions#create'
   delete  '/logout', to: 'sessions#destroy'
